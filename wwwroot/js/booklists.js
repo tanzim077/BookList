25d0d7b2ea57a9c24e1700de497f7828345410c4
tanzim.ahmed1994 <tanzim.ahmed1994@outlook.com>
2020-11-09 11:52:15 +0600

    Javascript Update

--- wwwroot/js/booklists.js
+++ wwwroot/js/booklists.js
@@ -1,27 +1,75 @@
-﻿var dataTable
+﻿var dataTable;
+
 $(document).ready(function () {
     loadDataTable();
 });
 
 function loadDataTable() {
-    dataTable = $('#DT_Load').dataTable({
+    dataTable = $('#DT_load').DataTable({
         "ajax": {
             "url": "/api/book",
             "type": "GET",
             "datatype": "json"
         },
         "columns": [
-            {"data":"name","width":"30%"},
-            {"data":"author","width":"30%"},
-            { "data": "isbn", "width": "30%" },
+            { "data": "name", "width": "20%" },
+            { "data": "author", "width": "20%" },
+            { "data": "isbn", "width": "20%" },
             {
                 "data": "id",
                 "render": function (data) {
-                    return `<div class="text-center"> </div>
-                    <a href >
-                `
-                }
+                    return `<div class="text-center">
+                        <a href="/BookList/Edit?id=${data}" class="btn btn-success text-white" style="cursor:pointer; width:70px;">
+                            Edit
+                        </a>
+                        &nbsp;
+                        <a class="btn btn-danger text-white" style="cursor:pointer; width:70px;">
+                            Delete
+                        </a>
+                        </div>`;
+                }, "width": "40%"
             }
-        ]
-    })
+        ],
+        "language": {
+            "emptyTable": "no data found"
+        },
+        "width": "100%"
+    });
 }
+
+// var dataTable;
+// $(document).ready(function () {
+//     loadDataTable();
+// });
+
+// function loadDataTable() {
+//     dataTable = $('#DT_load').DataTable({
+//         "ajax": {
+//             "url": "/api/book",
+//             "type": "GET",
+//             "datatype": "json"
+//         },
+//         "columns": [
+//             {"data": "name","width":"30%"},
+//             {"data": "author","width":"30%"},
+//             { "data": "isbn", "width": "30%" },
+//             {
+//                 "data": "id",
+//                 "render": function (data) 
+//                 {
+//                     return `<div class="text-center"> 
+//                     <a href ="/Booklist/Edit?id=${data}" class='btn btn success text white' style='cursor:pointer; width:100px;'> Edit</a>
+//                     &nbsp;
+//                     <a class='btn btn danger text white' style='cursor:pointer; width:100px;'> Delete</a>
+//                     </div>`;
+//                 },
+//                 "width":"30%"
+//             }
+//         ],
+//         "language" : 
+//         {
+//             "emptyTable" : "No data found"
+//         },
+//         "width": "100%"
+//     });
+// }
